1be44129dbaa225a9be858fc61de30db9ae3915e
Rich Samartino <rich@detroitcitycentral.com>
2017-04-03 22:15:23 -0400

    1st attempt at api player

--- index.php
+++ index.php
@@ -1,5 +1,153 @@
 <?php
 
-echo('Hello world!');
+/**
+ * Library Requirements
+ *
+ * 1. Install composer (https://getcomposer.org)
+ * 2. On the command line, change to this directory (api-samples/php)
+ * 3. Require the google/apiclient library
+ *    $ composer require google/apiclient:~2.0
+ */
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+  throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ .'"');
+}
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+$htmlBody = <<<END
+<form method="GET">
+  <div>
+    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
+  </div>
+  <div>
+    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
+  </div>
+  <input type="submit" value="Search">
+</form>
+END;
+
+// This code will execute if the user entered a search query in the form
+// and submitted the form. Otherwise, the page displays the form above.
+if (isset($_GET['q']) && isset($_GET['maxResults'])) {
+  /*
+   * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+   * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
+   * Please ensure that you have enabled the YouTube Data API for your project.
+   */
+  $DEVELOPER_KEY = 'AIzaSyByia2BjNIulfdLFHtwGeTbAxTLt8MMbEE';
+
+  $client = new Google_Client();
+  $client->setDeveloperKey($DEVELOPER_KEY);
+
+  // Define an object that will be used to make all API requests.
+  $youtube = new Google_Service_YouTube($client);
+
+  $htmlBody = '';
+  try {
+
+    // Call the search.list method to retrieve results matching the specified
+    // query term.
+    $searchResponse = $youtube->search->listSearch('id,snippet', array(
+      'q' => $_GET['q'],
+      'maxResults' => $_GET['maxResults'],
+    ));
+
+    $videos = '';
+    $channels = '';
+    $playlists = '';
+
+    // Add each result to the appropriate list, and then display the lists of
+    // matching videos, channels, and playlists.
+//     var_dump($searchResponse['items']);
+    foreach ($searchResponse['items'] as $searchResult) {
+      switch ($searchResult['id']['kind']) {
+        case 'youtube#video':
+          $videos .= sprintf('<li>%s (%s) - %s</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['videoId'], date_format(date_create($searchResult['snippet']['publishedAt']), "m/d/Y"));
+          break;
+        case 'youtube#channel':
+          $channels .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['channelId']);
+          break;
+        case 'youtube#playlist':
+          $playlists .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['playlistId']);
+          break;
+      }
+    }
+
+    $htmlBody .= <<<END
+    <h3>Videos</h3>
+    <ul>$videos</ul>
+    <h3>Channels</h3>
+    <ul>$channels</ul>
+    <h3>Playlists</h3>
+    <ul>$playlists</ul>
+END;
+  } catch (Google_Service_Exception $e) {
+    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  }
+}
 ?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>YouTube Search</title>
+  </head>
+  <body>
+    <?=$htmlBody?>
+    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+    <div id="player"></div>
+
+    <script>
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'to-dnlRsUUU',
+          events: {
+            'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+//         event.target.playVideo();
+		event.target.setPlaybackQuality('hd720');
+
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+//       var done = false;
+//       function onPlayerStateChange(event) {
+//         if (event.data == YT.PlayerState.PLAYING) {
+// //           setTimeout(stopVideo, 6000);
+// // 			setPlaybackQuality('small');
+// //           done = true;
+//         }
+//       }
+//       function stopVideo() {
+//         player.stopVideo();
+//       }
+    </script>
+  </body>
+</html>
